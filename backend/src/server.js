20789f810b264f6b60bcc31b68a270da919f5dbd
harshalDev <harshalwarade344@gmail.com>
2026-03-14 16:09:45 +0530

    create db models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,13 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDatabase } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is up and running."));
+connectDatabase();
+
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+
+app.listen(ENV.PORT, () => console.log("Server is up and running."));
